81a5cdbbd5d089bded2c375d2c436871ede6cd01
halas98 <132351477+halas98@users.noreply.github.com>
2023-09-05 21:28:06 +0200

    Update 11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b == 0:
-        return 1
-    elif b < 0:
-        return 1 / pow(a, -b)
-    else:
-        result = 1
-        for _ in range(b):
-            result *= a
-        return result
-if __name__ == "__main__":
-    print(pow(2, 2))
-    print(pow(98, 2))
-    print(pow(98, 0))
-    print(pow(100, -2))
-    print(pow(-4, 5))
+    c = a ** b
+    return c
