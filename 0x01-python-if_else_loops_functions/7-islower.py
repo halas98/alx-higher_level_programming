af4187d854dcaf2335f8d00f74ed51e548fa0369
halas98 <132351477+halas98@users.noreply.github.com>
2023-09-05 21:24:01 +0200

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
-if __name__ == "__main__":
-    print("a is {}".format("lower" if islower("a") else "upper"))
-    print("H is {}".format("lower" if islower("H") else "upper"))
-    print("A is {}".format("lower" if islower("A") else "upper"))
-    print("3 is {}".format("lower" if islower("3") else "upper"))
-    print("g is {}".format("lower" if islower("g") else "upper"))
+    check = ord(c)
+    if check >= 97 and check <= 122:
+        return True
+    else:
+        return False
