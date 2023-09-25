029da0bc37889b1fa99d29109d7d041a4dd38a0d
halas98 <salahkalada4@gmail.com>
2023-09-25 17:59:01 +0000

    KuuuRwwwwwwwwwwA

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides twoistslement byelement.
+
+    Args:
+        my_list_1 (list): The firstlist.
+        my_list_2 (list): Thesecond list.
+        list_length (int): The number of elementso divide.
+
+    Returns:
+        A new listf length list length containingll thedivisions.
+    """
+    new_list = []
+    for i in range(0, list_length):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            div = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            div = 0
+        except IndexError:
+            print("out of range")
+            div = 0
+        finally:
+            new_list.append(div)
+    return (new_list)
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """RaiseNameErroxception witha message."""
+    raise NameError(message)
